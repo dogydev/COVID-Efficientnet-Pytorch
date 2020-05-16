928ad4929127a8a89289896e91c4cc8c6dcfd261
dogydev <41112844+dogydev@users.noreply.github.com>
2020-05-16 11:48:44 +0100

    Create architecture.py

---
+++ model/architecture.py
@@ -0,0 +1,19 @@
+import torch
+from torch import nn
+from torchvision import models
+from .efficientnet import EfficientNet
+from .layers import Trainable, ConvBn2d
+
+
+class COVIDNext50(nn.Module):
+    def __init__(self, n_classes):
+        super(COVIDNext50, self).__init__()
+        self.n_classes = n_classes
+        trainable = True
+        self.efficientnet = EfficientNet(7, pretrained=True)
+
+    def forward(self, input):
+        return self.efficientnet(input)
+
+    def probability(self, logits):
+        return nn.functional.softmax(logits, dim=-1)
