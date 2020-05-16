3d3af30ae4c94faa49cc6a94c87352a44af4faaf
dogydev <41112844+dogydev@users.noreply.github.com>
2020-05-16 18:37:01 +0100

    Update minimal_prediction.py

--- minimal_prediction.py
+++ minimal_prediction.py
@@ -9,23 +9,28 @@ from model.architecture import COVIDNext50
 from data.transforms import val_transforms
 
 import config
+import os
 
 rev_mapping = {idx: name for name, idx in config.mapping.items()}
 
 model = COVIDNext50(n_classes=len(rev_mapping))
 
-ckpt_pth = './experiments/ckpts/COVIDNext50_NewData_F1_94.36_step_11200.pth'
+ckpt_pth = './experiments/ckpts/your_ckpt.path'
 weights = torch.load(ckpt_pth)['state_dict']
 model.load_state_dict(weights)
 model.eval()
 
 transforms = val_transforms(width=config.width, height=config.height)
-while True:
-	img_pth = input("Drag image here: ").replace("'", "")
-	img = Image.open(img_pth).convert("RGB")
-	img_tensor = transforms(img).unsqueeze(0)
-	
-	with torch.no_grad():
-	    logits = model(img_tensor)
-	    cat_id = int(torch.argmax(logits))
-	print("Prediction for {} is: {}".format(img_pth, rev_mapping[cat_id]))
+
+image_dir = "assets"
+images = os.listdir(image_dir)
+
+for image in images:
+    img_pth = image_dir + image
+    img = Image.open(img_pth).convert("RGB")
+    img_tensor = transforms(img).unsqueeze(0)
+
+    with torch.no_grad():
+        logits = model(img_tensor)
+        cat_id = int(torch.argmax(logits))
+    print("Prediction for {} is: {}".format(img_pth, rev_mapping[cat_id]))
