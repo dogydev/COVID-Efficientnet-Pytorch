674a98febc2bf511eda5913afdae3a5b493937fd
dogydev <41112844+dogydev@users.noreply.github.com>
2020-05-19 13:06:12 +0100

    Cleaned up code

--- model/architecture.py
+++ model/architecture.py
@@ -1,13 +1,11 @@
-import torch
 from torch import nn
-from torchvision import models
 from .efficientnet import EfficientNet
+
 
 class COVIDNext50(nn.Module):
     def __init__(self, n_classes):
         super(COVIDNext50, self).__init__()
         self.n_classes = n_classes
-        trainable = True
         self.efficientnet = EfficientNet(7, pretrained=True)
 
     def forward(self, input):
